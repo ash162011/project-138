468894bb65b0fdb436577f0b6cd959fccbb7abe3
ash162011 <83339498+ash162011@users.noreply.github.com>
2021-12-10 21:18:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup(){
+  var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+  console.log('Model Loaded!');
+}
+
+function draw(){
+  image(video, 0, 0, 700, 600);
+}
