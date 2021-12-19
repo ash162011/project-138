ecfd1c7eda0b69f88d60fb8ab670f4de5d8193ff
ash162011 <83339498+ash162011@users.noreply.github.com>
2021-12-19 14:15:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+rightWristX = 0;
+rightWristY = 0;
+scoreRightWrist = 0;
+
 function setup(){
 canvas =  createCanvas(600,500);
 canvas.parent('canvas');
@@ -13,11 +17,31 @@ function modelLoaded() {
   console.log('PoseNet Is Initialized');
 }
 
-function gotPoses(){
+function gotPoses(results){
+	if(results.length > 0){
+		rightWristX = results[0].pose.nose.x;
+	  rightWristY = results[0].pose.nose.y;
+	  console.log(results);
 
+	}
+}
+game_status = "";
+
+function startGame(){
+  game_status = "start";
+  document.getElementById("status").innerHTML = "Game Is Loaded";
 }
 
 function draw(){
   image(video, 0, 0, 700, 600);
-}
 
+  if(scoreRightWrist > 0.2){
+    fill("#ffdde4");
+    stroke("#ffdde4");
+    circle(rightWristX, rightWristY, 30);
+  }
+
+  if(game_status == "start"){
+    
+  }
+}
